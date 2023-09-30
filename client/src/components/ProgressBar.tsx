7cd031cf01cae0a9626a5cb8d92e0b0e2cfb4f8a
Tina Chen <tinachen2025@u.northwestern.edu>
2023-09-30 03:58:54 -0500

    added function name

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ProgressBar.css'
 
-export default function(props : any) {
+export default function ProgressBar(props : any) {
     const { current, total , cat } = props 
 
+    // multiple classes in same area will increment count but not progress bar t.f. breadth bar progress may not match numbers
     const progress = cat === 'Breadth' 
-        ? Object.values(props.counts).map((value : any) => value >= 1 ? 20 : 0).reduce((a : any, b : any) => a + b, 0) 
+        ? Object.values(props.counts).map((value : any) => value >= 1 ? 20 : 0).reduce((a : number, b : number) => a + b, 0) 
         : Math.min(current / total * 100, 100)
 
     return (
