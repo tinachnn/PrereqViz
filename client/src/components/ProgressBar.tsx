858ebfc3535f1bb6e53d92c3e1020572f068a243
Tina Chen <tinachen2025@u.northwestern.edu>
2023-09-30 02:26:27 -0500

    progress bar cannot go beyond 100%

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -4,7 +4,7 @@ import './ProgressBar.css'
 export default function(props : any) {
     const current : number = props.current;
     const total : number = props.total;
-    const progress = current / total * 100;
+    const progress = Math.min(current / total * 100, 100);
     return (
         <div className="progress-container">
             <div className="progress-bar" style={{ width: `${progress}%` }}>
