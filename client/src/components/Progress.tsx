727f8c5281087e5f710e1a80b0ae1d9bd8d4c7be
Tina Chen <tinachen2025@u.northwestern.edu>
2023-09-30 03:30:15 -0500

    fixed progress bar

--- client/src/components/Progress.tsx
+++ client/src/components/Progress.tsx
@@ -4,38 +4,28 @@ import ProgressBar from './ProgressBar';
 
 export default function Progress(props : any) {
     const { progress } = props;
+
     console.log('PROGRESS')
     console.log(progress)
     const counts : any = {
-        'core' : {
-            'current' : 0,
-            'total' : 6
-        },
-        'project' : {
-            'current' : 0,
-            'total' : 2
-        },
-        'technical' : {
-            'current' : 0,
-            'total' : 6
-        },
-        'related' : {
-            'current' : 0,
-            'total' : 5
-        }
+        'core' : 0, 
+        'project' : 0,
+        'technical' : 0,
+        'related' : 0,
+        'breadth' : {}
     }
-    const breadth = ['software', 'theory', 'ai', 'interfaces', 'systems'];
-    breadth.forEach((area : string) => counts[area] = { 'current' : 0 , 'total' : 1 })
 
-    progress.forEach((item : { id : number , area : string}) => counts[item.area].current += 1)
+    const breadths = ['software', 'theory', 'ai', 'interfaces', 'systems'];
+    breadths.forEach((area : string) => counts.breadth[area] = 0)
+    progress.forEach((item : { id : number , area : string}) => breadths.includes(item.area) ? counts.breadth[item.area] += 1 : counts[item.area] += 1)
 
     return (
         <div className="progress">
-            <ProgressBar current={ counts.core.current } total={ counts.core.total } cat='Required'/>
-            <ProgressBar current={ counts.software.current + counts.theory.current + counts.ai.current + counts.interfaces.current + counts.systems.current } total={ counts.software.total + counts.theory.total + counts.ai.total + counts.interfaces.total + counts.systems.total } cat='Breadth'/>
-            <ProgressBar current={ counts.project.current } total={ counts.project.total } cat='Project'/>
-            <ProgressBar current={ counts.technical.current } total={ counts.technical.total } cat='Technical'/>
-            <ProgressBar current={ counts.related.current } total={ counts.related.total } cat='Related'/>
+            <ProgressBar current={ counts.core } total={6} cat='Required'/>
+            <ProgressBar current={ Object.values(counts.breadth).reduce((a : any, b : any) => a + b, 0) } total={5} counts={ counts.breadth } cat='Breadth'/>
+            <ProgressBar current={ counts.project } total={2} cat='Project'/>
+            <ProgressBar current={ counts.technical } total={6} cat='Technical'/>
+            <ProgressBar current={ counts.related } total={5} cat='Related'/>
         </div>
     )
 }
--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './ProgressBar.css'
 
 export default function(props : any) {
-    const current : number = props.current;
-    const total : number = props.total;
-    const progress = Math.min(current / total * 100, 100);
+    const { current, total , cat } = props 
+
+    const progress = cat === 'Breadth' 
+        ? Object.values(props.counts).map((value : any) => value >= 1 ? 20 : 0).reduce((a : any, b : any) => a + b, 0) 
+        : Math.min(current / total * 100, 100)
+
     return (
         <div className="progress-container">
             <div className="progress-bar" style={{ width: `${progress}%` }}>
                 <div className="text-container">
-                    <span className="text" >{ current }/{ total } {props.cat}</span>
+                    <span className="text" >{ current }/{ total } {cat}</span>
                 </div>
             </div>
         </div>
